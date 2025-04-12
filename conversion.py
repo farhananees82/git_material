5000e0b632f4f918663c36d276885f4dcc3128f0
mfarhananees <mfarhan.anees@hotmail.com>
2025-04-12 22:36:32 +0500

    Files updated

---
+++ conversion.py
@@ -0,0 +1,7 @@
+# Scenario 
+# usd, gbp, pkr, value 
+
+
+pkr = int(input("Enter the pkr amount: "))
+print(f'Your amount in USD: {pkr_to_usd(pkr)}')
+print(f'Your amount in GBP: {pkr_to_gbp(pkr)}')
