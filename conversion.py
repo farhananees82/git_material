a5ddd0cb9dffd8dcb9a6e6d92494008f34315e04
mfarhananees <mfarhan.anees@hotmail.com>
2025-04-12 22:40:37 +0500

    GBP conversion added

--- conversion.py
+++ conversion.py
@@ -1,6 +1,11 @@
 # Scenario 
 # usd, gbp, pkr, value 
+# pkr to gbp -> 
 
+def pkr_to_gbp(pkr):
+    rate = 300
+    amount = pkr / rate
+    return amount
 
 pkr = int(input("Enter the pkr amount: "))
 print(f'Your amount in USD: {pkr_to_usd(pkr)}')
